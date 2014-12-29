693dd2171002881aea3d4a3773c5d85779331f0b
zoe <zoe@zoedeMacBook-Air.local>
2014-12-29 22:36:47 +0800

    First Commit@ 2014/12/29

---
+++ RandomLineChart/RandomLineChart/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RandomLineChart
+//
+//  Created by zoe on 14/12/29.
+//  Copyright (c) 2014年 LivinSpring. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
